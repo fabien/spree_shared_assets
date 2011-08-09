ada68f9eab0ebd6272c5a5b09c1fbc8c406c84c6
Fabien Franzen <info@atelierfabien.be>
2011-08-09 22:47:55 +0200

    Explicit include of :asset association

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,5 +1,5 @@
 Product.class_eval do
-  has_many :assets_shares, :dependent => :destroy, :as => :shareable
+  has_many :assets_shares, :dependent => :destroy, :as => :shareable, :include => :asset
   has_many :images, :source => :asset, :foreign_key => "asset_id", :through => :assets_shares, :class_name => "Image", :order => "assets.position ASC"
   
   def all_images
--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -1,4 +1,4 @@
 Variant.class_eval do
-  has_many :assets_shares, :dependent => :destroy, :as => :shareable
+  has_many :assets_shares, :dependent => :destroy, :as => :shareable, :include => :asset
   has_many :images, :source => :asset, :foreign_key => "asset_id", :through => :assets_shares, :class_name => "Image", :dependent => :destroy, :order => "assets.position ASC"
 end
