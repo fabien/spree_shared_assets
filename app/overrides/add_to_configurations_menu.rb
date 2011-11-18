c2ca5151c6bf7287e09691e25f3e1576dc918588
Fabien Franzen <info@atelierfabien.be>
2011-11-18 14:29:49 +0100

    Overrides now working again (don't put them in app/overrides/admin)

--- app/overrides/add_to_configurations_menu.rb
+++ app/overrides/add_to_configurations_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => "admin/configurations/index",
-  :name => "currency_exchange_admin_configurations_menu",
+  :name => "shared_assets_admin_configurations_menu",
   :insert_bottom => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
   :text => "<%= configurations_menu_item(t('shared_asset_settings.title'), admin_shared_asset_settings_path, t('shared_asset_settings.description')) %>",
   :disabled => false)
