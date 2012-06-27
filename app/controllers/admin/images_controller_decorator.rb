8c8081a38e4fdf658fcbdff3fd83f13cce230a2a
Fabien Franzen <info@atelierfabien.be>
2012-06-27 18:18:05 +0200

    Fixed sortable behaviour (use spree_dom_id instead of dom_id)

--- app/controllers/admin/images_controller_decorator.rb
+++ app/controllers/admin/images_controller_decorator.rb
@@ -11,21 +11,24 @@ module Spree
       def set_viewable
         @image.viewable = @product
         
-        if params[:shareable_id] && !params[:shareable_id].blank? # Check if "Product" option was selected
+        assign_to_product = params[:shareable_id] && !params[:shareable_id].blank?
+        if assign_to_product # Check if "Product" option was selected
           # Assign to product
           @image.products << @product unless @image.products.include?(@product)
           params.delete(:shareable_id)
-        else # "Product" option not selected
-          # Check if at least one variant selected, if not assign to "Product"
-          variant_ids = (params[:variant_ids] || []).select { |i| !i.blank? }
-          if variant_ids.any?
-            # DISABLED: "Product" option not selected, delete if it exists 
-            # shares = @image.assets_shares.find_all_by_shareable_type('Product')
-            # shares.each{|s| s.destroy if s.shareable_id == @product.id} if shares.size > 0
-            @image.variant_ids = variant_ids
-          else # no variant ids specified
-            @image.products << @product unless @image.products.include?(@product)
-          end
+        else
+          # "Product" option not selected, delete if it exists 
+          shares = @image.assets_shares.find_all_by_shareable_type('Spree::Product')
+          shares.each { |s| s.destroy if s.shareable_id == @product.id } if shares.size > 0
+        end  
+        
+        # Check if at least one variant selected, if not assign to "Product"
+        variant_ids = (params[:variant_ids] || []).select { |i| !i.blank? }
+        if variant_ids.any?
+          @image.variant_ids = variant_ids
+        elsif !assign_to_product
+          # No variants or Product specified, default to at least product assignment
+          @image.products << @product unless @image.products.include?(@product)
         end
       end
 
